506369ba2b39aa901f98b05db9213abbdc06aef5
LanaTch <tchobanou.sg@gmail.com>
2019-01-05 13:45:42 +0300

    окончательный вариант задач

---
+++ Lesson3_t5_hw.py
@@ -0,0 +1,19 @@
+# 5. В массиве найти максимальный отрицательный элемент.
+# Вывести на экран его значение и позицию в массиве.
+
+import random
+
+SIZE = 10
+MIN_ITEM = -10
+MAX_ITEM = 10
+array = [random.randint(MIN_ITEM, MAX_ITEM) for _ in range(SIZE)]
+print(array)
+
+max_neg = MIN_ITEM
+pos_max_neg = 0
+for i in range(SIZE):
+    if array[i] < 0 and abs(array[i]) < abs(max_neg):
+        max_neg = array[i]
+        pos_max_neg = i
+
+print(f'максимальный отрицательный элемент = {max_neg}\nего позиция в массиве = {pos_max_neg}')
---
+++ Lesson3_t8_hw.py
@@ -0,0 +1,21 @@
+# 8. Матрица 5x4 заполняется вводом с клавиатуры кроме последних элементов строк.
+# Программа должна вычислять сумму введенных элементов каждой строки и
+# записывать ее в последнюю ячейку строки. В конце следует вывести полученную матрицу.
+
+SIZE_COL = 4
+SIZE_ROW = 5
+matrix = []
+
+for i in range(SIZE_ROW):
+    summa = 0
+    row = []
+    for j in range(SIZE_COL):
+        row.append(int(input(f'введите элемент матрицы ({i},{j}):')))
+        summa += row[j]
+    row.append(summa)
+    matrix.append(row)
+
+for i in range(SIZE_ROW):
+    for j in range(SIZE_ROW):
+        print(matrix[i][j], end='\t')
+    print()
