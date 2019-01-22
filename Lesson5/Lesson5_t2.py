0b90777e03961edc07abc6ee0a213ae3eb09f509
LanaTch <tchobanou.sg@gmail.com>
2019-01-22 10:59:08 +0300

    update create dicts in t2

--- Lesson5/Lesson5_t2.py
+++ Lesson5/Lesson5_t2.py
@@ -5,10 +5,8 @@
 
 from collections import deque
 
-hex_dict = {'0': 0, '1': 1, '2': 2, '3': 3, '4': 4, '5': 5, '6': 6, '7': 7, '8': 8, '9': 9,
-            'A': 10, 'B': 11, 'C': 12, 'D': 13, 'E': 14, 'F': 15}
-hex_dict_reverse = {0: '0', 1: '1', 2: '2', 3: '3', 4: '4', 5: '5', 6: '6', 7: '7', 8: '8', 9: '9',
-                    10: 'A', 11: 'B', 12: 'C', 13: 'D', 14: 'E', 15: 'F'}
+hex_dict = dict(zip('0123456789ABCDEF', range(16)))
+hex_dict_reverse = dict(zip(range(16), '0123456789ABCDEF'))
 
 def sum_hex_func(a, b):
     len_a, len_b = len(a), len(b)
